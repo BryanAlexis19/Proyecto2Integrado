9e54dc4fde98b902f7c4e3782ce5fd87035be6e6
= <cremazter@hotmail.com>
2022-06-12 19:00:03 -0500

    actualizacion12/06/20202

---
+++ SistemaSGDP/src/main/java/Modelo/modelo_usuario.java
@@ -0,0 +1,30 @@
+
+package Modelo;
+
+public class modelo_usuario {
+    private String usuario;
+    private String contraseña;
+
+    public modelo_usuario(String usuario, String contraseña) {
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+    
+}
