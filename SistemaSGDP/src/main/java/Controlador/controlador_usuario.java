2794b74c74f770ac4e8f1893ac90df19dc61a4ac
= <cremazter@hotmail.com>
2022-06-15 00:25:48 -0500

    15/06/2022-2

--- SistemaSGDP/src/main/java/Controlador/controlador_usuario.java
+++ SistemaSGDP/src/main/java/Controlador/controlador_usuario.java
@@ -1,10 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Controlador;
 
+import Conexion.Conexion;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import Modelo.*;
+import java.sql.Connection;
+import java.util.ArrayList;
 
-public class controlador_usuario {
+public class controlador_usuario implements Interface.interfaz_usuario {
+
+    @Override
+    public List<modelo_usuario> ULista() {
+       List<modelo_usuario> Lista = new ArrayList();
+     try{
+       Connection cn= Conexion.getConnection();
+       String sql="SELECT idUsuario, UserName, Password, Tipo FROM usuario";
+       PreparedStatement st = cn.prepareStatement(sql);
+       ResultSet rs= st.executeQuery();
+       while (rs.next()){
+           modelo_usuario x = new modelo_usuario (); 
+           x.setId_usuario(rs.getInt(1));
+           x.setUsuario(rs.getString(2));
+           x.setContraseña(rs.getString(3));
+           x.setTipo(rs.getInt(4));
+           Lista.add(x);
+       }
+     }catch(Exception x){}
+     return Lista;
     
-}
+    }
+    
+    
+    
+    }
+
---
+++ SistemaSGDP/src/main/java/Interface/interfaz_usuario.java
@@ -0,0 +1,12 @@
+
+package Interface;
+
+import Modelo.*;
+import java.util.*;
+
+public interface interfaz_usuario {
+    
+    List<modelo_usuario> ULista = new ArrayList();
+    List<modelo_usuario> ULista();
+    
+}
